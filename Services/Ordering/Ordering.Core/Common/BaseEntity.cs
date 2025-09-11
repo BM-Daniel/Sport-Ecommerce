600506eaf441b1b434f5910d178a76afd95669f0
BM-Daniel <danbabereyir@gmail.com>
2025-09-11 22:09:41 +0000

    i am back - complete infrastructure domain

---
+++ Services/Ordering/Ordering.Core/Common/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Ordering.Core.Common
+{
+    public class BaseEntity
+    {
+        // can be created in the derived class
+        public int Id { get; protected set; }
+        // would be created in the parent class - here
+        public string? CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string? LastModifiedBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
---
+++ Services/Ordering/Ordering.Infrastructure/Data/OrderContextSeed.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Core.Entities;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContextSeed
+    {
+        public static async Task SendAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation($"Ordering Database: {typeof(OrderContext).Name} seeded!!");
+            }
+        }
+
+        private static IEnumerable<Order> GetOrders()
+        {
+            return new List<Order>
+            {
+                new ()
+                {
+                    UserName = "danzzy",
+                    FirstName = "Danzzy",
+                    LastName = "Tech",
+                    EmailAddress = "danzzytech@mailinator.com",
+                    AddressLine = "Bamvim",
+                    Country = "Ghana",
+                    TotalPrice = 800,
+                    State = "Northern Region",
+                    ZipCode = "233",
+                    CardName = "Visa",
+                    CardNumber = "4531000008004567",
+                    CreatedBy = "Danzzy",
+                    Expiration = "12/29",
+                    Cvv = "456",
+                    PaymentMethod = 1,
+                    LastModifiedBy = "Danzzy",
+                    LastModifiedDate = new DateTime()
+                }
+            };
+        }
+    }
+}

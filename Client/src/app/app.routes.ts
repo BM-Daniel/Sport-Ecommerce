0c797f81c909e1c094189eaa794e0ef07349f5bd
BM-Daniel <danbabereyir@gmail.com>
2025-10-07 22:04:13 +0000

    implement error handling for client using interceptors

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ServerErrorComponent } from './core/server-error/server-error.component';
+import { NotFoundComponent } from './core/not-found/not-found.component';
+import { NotAuthenticatedComponent } from './core/not-authenticated/not-authenticated.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -8,5 +11,8 @@ export const routes: Routes = [
     loadChildren: () =>
       import('./store/store.module').then((mod) => mod.StoreModule),
   },
+  { path: 'server-error', component: ServerErrorComponent },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: 'not-authenticated', component: NotAuthenticatedComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
